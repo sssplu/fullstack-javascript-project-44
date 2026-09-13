7faef1d225b7a5ca6fcb3a6694dbb9ebead50acb
Yasenia <yaseniya63@gmail.com>
2026-09-13 14:21:50 +0000

    style: lint-fix gcd.js and progression.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,18 +3,18 @@ import _ from 'lodash';
 export const description = 'Find the greatest common divisor of given numbers.';
 
 export default () => {
-let n1 = _.random(0, 100);
-let n2 = _.random(0, 100); 
+  let n1 = _.random(0, 100);
+  let n2 = _.random(0, 100); 
 
-const task = `${n1} ${n2}`;
+  const task = `${n1} ${n2}`;
 
-while (n2 !== 0) {
-const temp = n2;
-n2 = n1 % n2;
-n1 = temp;
-}
+  while (n2 !== 0) {
+    const temp = n2;
+    n2 = n1 % n2;
+    n1 = temp;
+  }
 
-const result = `${n1}`;
+  const result = `${n1}`;
 
-return [task, result];
+  return [task, result];
 };
--- src/games/progression.js
+++ src/games/progression.js
@@ -4,28 +4,28 @@ export const description = 'What number is missing in the progression?';
 
 const createProgression = () => {
 
-const startNumber = _.random(0, 100);
-const index = _.random(0, 100);
-const maxStep = _.random(5, 10);
+  const startNumber = _.random(0, 100);
+  const index = _.random(0, 100);
+  const maxStep = _.random(5, 10);
 
-const progression = [startNumber];
+  const progression = [startNumber];
 
-for (let step = 1; step < maxStep; step++) {
-const currentElement = startNumber + index * step;
-progression.push(currentElement);
-}
+  for (let step = 1; step < maxStep; step++) {
+    const currentElement = startNumber + index * step;
+    progression.push(currentElement);
+  }
 
-return progression;
+  return progression;
 };
 
 export default () => {
-const progression = createProgression(); 
-const randomStep = _.random(0, progression.length - 1);  
+  const progression = createProgression(); 
+  const randomStep = _.random(0, progression.length - 1);  
 
-const result = `${progression[randomStep]}`;
+  const result = `${progression[randomStep]}`;
 
-progression[randomStep] = '..';
-const task = progression.join(' ');
+  progression[randomStep] = '..';
+  const task = progression.join(' ');
 
-return [task, result];
+  return [task, result];
 };
